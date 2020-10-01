1f687b20c3c1ac83a488f3e251dce59ec6f3fc73
Vlad <vgayduk82@gmail.com>
2020-10-01 13:11:48 +0300

    Improve Lecture 6 HW

--- lecture 6/scripts/functions.js
+++ lecture 6/scripts/functions.js
@@ -1,7 +1,8 @@
-function createErrorMessage() {
-	let errDiv = document.createElement('div');
+function createErrorMessage(container) {
+  let errDiv = document.createElement("div");
 
-	container.appendChild(errDiv);
-	errDiv.innerHTML = 'Ooops... Something went wrong...<br>Try to reenter the name';
-	errDiv.classList.add('error');
+  container.appendChild(errDiv);
+  errDiv.innerHTML =
+    "Ooops... Something went wrong...<br>Try to reenter the name";
+  errDiv.classList.add("error");
 }
--- lecture 6/scripts/script.js
+++ lecture 6/scripts/script.js
@@ -1,12 +1,6 @@
-const server = 'http://api.tvmaze.com/search/shows/?q=';
-const container = document.querySelector('#content-container');
-const input = document.querySelector('#content-name');
-const searchBtn = document.querySelector('#search-btn');
-let isInputFocused;
+const server = "http://api.tvmaze.com/search/shows/?q=";
+const container = document.querySelector("#content-container");
+const form = document.querySelector("#search");
+const input = form.contentName;
 
-searchBtn.addEventListener('click', findContent);
-input.addEventListener('focus', () => isInputFocused = true);
-input.addEventListener('blur', () => isInputFocused = false);
-document.addEventListener('keydown', (e) => {
-	if (isInputFocused && e.key == 'Enter') findContent();
-});
+form.addEventListener("submit", e => findContent(e, server, container));
